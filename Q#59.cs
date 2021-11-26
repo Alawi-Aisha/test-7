085a9305994137781837e4b3caeaf84bd56c0305
Alawi-Aisha <aishaalvi433@gmail.com>
2021-11-26 11:56:46 +0500

    first commit

---
+++ Q#59.cs
@@ -0,0 +1,61 @@
+using System;
+
+namespace OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          //(a)
+                int n=22/8;
+                Console.WriteLine(n);
+
+            //(b)
+                int n= 99/8 + 21/11*7;
+                Console.WriteLine(n);
+
+            //(c)
+                double a=4, b=3, p=8, q=2;
+                double n=q/a - p/b;
+                Console.WriteLine(n);
+
+            //(d)
+                int a=6, b=3, p=8, q=2;
+                double n=p/a + q/b;
+                Console.WriteLine(n);
+
+            //(e)
+                int a=3.3, b=2.7, p=8.7, q=5.4;
+                double n=p/b + q/a;
+                Console.WriteLine(n);
+
+            //(f)
+                int n=11+7/4+98%6*3;
+                Console.WriteLine(n);
+
+            //(g)
+                int n=11/31%8*5-12;
+                Console.WriteLine(n);
+
+            //(h)
+                double p=8;
+                int q=5;
+                int n=q*p + 3.0*p*p - q%3.0*p*p*p;
+                Console.WriteLine(n);
+
+            //(i)
+                double p=5;
+                int q=9;
+                int n=(q%2)p + 6.0*p(q%3) + (q%4)*p*p*p;
+                Console.WriteLine(n);
+
+            //(j)
+                int n=3, a=6, b=2;
+                n*= (a+b);
+                a--;
+                n=n-a;
+                Console.WriteLine(n);
+
+        }
+    }
+}
